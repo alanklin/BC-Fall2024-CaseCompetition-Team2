a646ed7057c7f0756649a6f631b2e20137046572
txpetrin <txpetrin@mtu.edu>
2024-10-14 15:22:16 -0400

    Imputed Pickle Data as Zip

--- Code/_Setup.py
+++ Code/_Setup.py
@@ -13,6 +13,12 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import random
+import pickle
+
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import KNNImputer, IterativeImputer, SimpleImputer
+
+from sklearn.cluster import KMeans
 
 from sklearn.model_selection import train_test_split
 
@@ -30,8 +36,16 @@ dataPath = os.path.join(projPath, "Data")
 
 rawDataPath = os.path.join(dataPath, "rawData")
 
-
+imputedDataPath = os.path.join(dataPath, "imputedData")
 
 
 rawTrainFile = os.path.join(rawDataPath, "train.csv")
 rawTestFile = os.path.join(rawDataPath, "test.csv")
+
+tensorDecompTrainFile = os.path.join(imputedDataPath, "tensor_decomp_pre_knn.csv")
+imputedTrainFile = os.path.join(imputedDataPath, "iterative_imputed.csv")
+kmeansTrainFile = os.path.join(imputedDataPath, "kmeans_imputed.csv")
+
+tensorDecompTrainFilePickle = os.path.join(imputedDataPath, "tensor_decomp_pre_knn.pkl")
+imputedTrainFilePickle = os.path.join(imputedDataPath, "iterative_imputed.pkl")
+kmeansTrainFilePickle = os.path.join(imputedDataPath, "kmeans_imputed.pkl")
